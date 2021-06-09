2fb376f65eb222a679f9f50490c3ad991f444144
rituparna-india1947 <85564017+rituparna-india1947@users.noreply.github.com>
2021-06-09 13:34:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function one() {
+    window.alert("The Taj Mahal is one of the most beautiful buildings in the world. It was built in 1631 on the banks of river Yamuna at Agra, India. About three hundred years ago, Emperor Shah jahan of Delhi build it in memory of his wife Mumtaz Mahal. It is made of pure white marble. As a monument of love 'it is unsurpassed in the world.' It stands on a platform of 8.5 meters height. The mausoleum rises to a height of 32.4 meters. It is surmounted by cupolas at each corner. The bulbous dome in the centre of the cupolas has the appearance of an inverted lotus. There are four smaller domes at the four corners of the building. Four minarets stand at each corner of the terrace. The outer walls and the interior walls are richly decorated with exquisite inlay work and calligraphy. The Taj Mahal is considered as one of the Seven Wonders of the World. Many tourists come to see this beautiful structure from different parts of the world.");
+  }
+  function two() {
+    window.alert("The Colosseum, also known as the Flavian Amphitheatre, is a large artefact or structure in the city of Rome. The construction of the Colosseum started around 70–72 AD and was finished in 80 AD. It is the biggest amphitheatre built by the Roman Empire. The Colosseum is in Rome, the capital of Italy.");
+  }
+  function three() {
+    window.alert("Christ the Redeemer is the largest art deco statue in the world. It is 98 feet tall (not including the 26 foot pedestal), and the arms stretch to 92 feet wide. The statue weighs approximately 635 tonnes. Christ the Redeemer is located in the Tijuca Forest National Forest, at the top of the Corcovado Mountain.");
+  }
+  function four() {
+    window.alert("Machu Picchu is a 15th-century Inca site located on a ridge between the Huayna Picchu and Machu Picchu mountains in Peru. It sits 7,970 feet (2,430 meters) above sea level on the eastern slope of the Andes and overlooks the Urubamba River hundreds of feet below.");
+  }
+  function five() {
+    window.alert("The wall is made of cement, rocks, bricks, and dirt. It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built. It is about 21,196 kilometers long, 9.1 metres (30 feet) wide and 15 metres high.");
+  }
+  function six() {
+    window.alert("Inhabited since prehistoric times, this Nabataean caravan-city, situated between the Red Sea and the Dead Sea, was an important crossroads between Abia, Egypt and Syria-Phoenicia. Petra is half-built, half-carved into the rock, and is surrounded by mountains riddled with passages and gorges. It is one of the world's most famous archaeological sites, where ancient Eastern traditions blend with Hellenistic architecture.");
+  }
+  function seven() {
+    window.alert("Chichén Itzá, ruined ancient Maya city occupying an area of 4 square miles (10 square km) in south-central Yucatán state, Mexico. It is thought to have been a religious, military, political, and commercial centre that at its peak would have been home to 35,000 people. The site first saw settlers in 550, probably drawn there because of the easy access to water in the region via caves and sinkholes in limestone formations, known as cenotes.");
+  }
